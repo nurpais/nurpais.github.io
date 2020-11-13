d7cf61e82926c654e4cd8f69c1d128798f2d483c
Nurpais Orozulbaev <orozulbaev@gmail.com>
2020-11-13 21:39:50 +0600

    small changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,36 @@
-window.addEventListener("load", function () {
-  // Locomotive scroll
-  (function () {
-    var scroll = new LocomotiveScroll({
-      el: document.querySelector("[data-scroll-container]"),
-      smooth: true,
+// Locomotive scroll
+(function () {
+  var scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+  });
+})();
+
+// GSAP cursor
+(function () {
+  let cursor = document.querySelector(".cursor");
+
+  function movecursor(e) {
+    TweenLite.to(cursor, 0.3, {
+      x: e.clientX,
+      y: e.clientY,
     });
-  })();
+  }
 
-  // GSAP cursor
-  (function () {
-    let cursor = document.querySelector(".cursor");
+  window.addEventListener("mousemove", movecursor);
 
-    function movecursor(e) {
-      TweenLite.to(cursor, 0.3, {
-        x: e.clientX,
-        y: e.clientY,
-      });
-    }
-
-    window.addEventListener("mousemove", movecursor);
-
-    document.querySelectorAll("a").forEach((el) => {
-      el.addEventListener("mouseenter", () => {
-        cursor.classList.add("active");
-      });
+  document.querySelectorAll("a").forEach((el) => {
+    el.addEventListener("mouseenter", () => {
+      cursor.classList.add("active");
     });
+  });
 
-    document.querySelectorAll("a").forEach((el) => {
-      el.addEventListener("mouseleave", () => {
-        cursor.classList.remove("active");
-      });
+  document.querySelectorAll("a").forEach((el) => {
+    el.addEventListener("mouseleave", () => {
+      cursor.classList.remove("active");
     });
-  })();
+  });
+})();
 
-  // Hero animation
-  (function () {})();
-});
+// Hero animation
+(function () {})();
