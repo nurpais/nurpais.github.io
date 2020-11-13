3f6b8274d9c99d465ca53a055723658ae32ff03b
Nurpais Orozulbaev <orozulbaev@gmail.com>
2020-11-13 21:23:51 +0600

    small js changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
   // Locomotive scroll
   (function () {
     var scroll = new LocomotiveScroll({
